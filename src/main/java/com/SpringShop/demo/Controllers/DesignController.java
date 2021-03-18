5de33bb3af6f6845bf12129d1dfcab0fc5181b2a
Kacper Kromka <kacper.kromkaa@gmail.com>
2021-03-18 16:31:34 +0100

    first commit

---
+++ src/main/java/com/SpringShop/demo/Controllers/DesignController.java
@@ -0,0 +1,44 @@
+package com.SpringShop.demo.Controllers;
+
+import com.SpringShop.demo.Design;
+import com.SpringShop.demo.Design.Type;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@Controller
+@Slf4j
+@RequestMapping("/design")
+public class DesignController {
+
+    @GetMapping
+    public String design(Model model) {
+        List<Design> designs = Arrays.asList(
+                new Design(1,"czarne kropki", Type.BLACKWHITE),
+                new Design(2, "czarne paski", Type.BLACKWHITE),
+                new Design(3, "kolorowe paski", Type.COLOR)
+        );
+
+        Type[] types = Design.Type.values();
+        for (Type type: types){
+            model.addAttribute(type.toString().toLowerCase(Locale.ROOT),filterByType(designs, type));
+        }
+        model.addAttribute("design", new Product());
+        return "design";
+    }
+
+    private List<Design> filterByType(List<Design> designs, Type type) {
+
+        return designs.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+
+    }
+}
---
+++ src/main/java/com/SpringShop/demo/Design.java
@@ -0,0 +1,16 @@
+package com.SpringShop.demo;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Design {
+    private final int id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        BLACKWHITE, COLOR
+    }
+}
